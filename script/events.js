2219e38524a62d09c86aa4aa99bb87a7c5af45e6
dhuron1980 <danielrhuron@gmail.com>
2016-07-08 19:05:21 -0500

    First Push
    This is my first save of this revamped project.

---
+++ script/events.js
@@ -0,0 +1,11 @@
+// This is where we input data for the event calendar
+
+var eventCal = [
+	["Monday, August 22","First day of the Fall Semester"],
+	["Monday, August 29","Last day to add a class"],
+	["Friday, Sept. 2","An example of a lunch and learn event presented by Dr. Firstname Lastname.", "2 p.m. in the KIVA."],
+	["Monday, Sept. 5","Labor Day holiday"],
+	["Tuesday, Nov. 8","U.S. Election Day! Get out and vote!","7 a.m. - 7 p.m. Your local voting location."],
+	["Thursday, Dec. 19","Official closing of Fall session"],
+	["Friday, Dec. 20","Another example of a longer entry that will allow me to test this function.", "5 p.m. in FH 490"]
+];
